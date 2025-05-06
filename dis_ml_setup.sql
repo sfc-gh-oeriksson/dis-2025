e86b848190c8e9223d0d32496d1a36e9bc96eda8
Oskar Eriksson <oskar.eriksson@snowflake.com>
2025-05-06 14:50:59 +0200

    Adding notebook creation and link at the end

--- dis_ml_setup.sql
+++ dis_ml_setup.sql
@@ -14,3 +14,16 @@ CREATE STAGE IF NOT EXISTS ML_HOL_DB.ML_HOL_SCHEMA.DIAMONDS_ASSETS
     FILE_FORMAT = ML_HOL_DB.ML_HOL_SCHEMA.CSVFORMAT 
     URL = 's3://sfquickstarts/intro-to-machine-learning-with-snowpark-ml-for-python/diamonds.csv';
     -- https://sfquickstarts.s3.us-west-1.amazonaws.com/intro-to-machine-learning-with-snowpark-ml-for-python/diamonds.csv
+
+-- Create Notebook
+CREATE OR REPLACE NOTEBOOK ML_HOL_DB.ML_HOL_SCHEMA.ML_HOL_DEMO_NOTEBOOK 
+FROM '@ML_HOL_DB.ML_HOL_SCHEMA.GITHUB_INTEGRATION_SNOWFLAKE_ML_HOL/branches/main/' 
+MAIN_FILE = 'GETTING_STARTED_WITH_ML.ipynb'
+QUERY_WAREHOUSE = 'ML_HOL_WH';
+
+ALTER NOTEBOOK ML_HOL_DB.ML_HOL_SCHEMA.ML_HOL_DEMO_NOTEBOOK ADD LIVE VERSION FROM LAST;
+
+-- Provide link to notebook
+SELECT 'Visit the demo notebook here: ' AS DEMO_INSTRUCTIONS
+UNION ALL
+SELECT CONCAT_WS('/', 'https://app.snowflake.com',CURRENT_ORGANIZATION_NAME(), CURRENT_ACCOUNT_NAME(), '#/notebooks/ML_HOL_DB.ML_HOL_SCHEMA.ML_HOL_DEMO_NOTEBOOK') AS DEMO_INSTRUCTIONS;
